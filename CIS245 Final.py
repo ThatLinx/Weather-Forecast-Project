ff2d86c2752808e17611ff46cc839785fd5378b2
ThatLinx <ysabir24@gmail.com>
2023-08-27 17:44:02 -0700

    Moved all methods to methods file, added loop to allow for searching of specified info

--- CIS245 Final.py
+++ CIS245 Final.py
@@ -1,17 +1,18 @@
-import requests, json
-
+import requests, json, os, time, sys
+import finalMethods as fm
 apiKey = "a6c36ee525113dfa0a012225959df0d9"
 
 def main():
     #Main Variables
     stillUsing = True
     locationValid = False
+    displayInformation = True
     
     #Main loop
     while stillUsing is True:
         #Validation loop to make sure that selected location exists
         while locationValid is False:
-            #Lines 11 to 20 get location and then convert it to Lat and Lon for accurate location information while checking whether to use Api call for zip or name
+            #Lines 15 to 25 get location and then convert it to Lat and Lon for accurate location information while checking whether to use Api call for zip or name
             requestedLocation = input("What city are you looking for? (Enter zip or city name and state e.g. Springfield, OR): ")
             try:
                 int(requestedLocation)    
@@ -25,7 +26,7 @@ def main():
 
 
 
-        #Lines 25-39 check to make sure location entered is not a jumble of letters, numbers, symbols, or empty strings. It also checks to see if Zip or City name and State is being used and gets the proper url for the forecast API call
+        #Lines 29-44 check to make sure location entered is not a jumble of letters, numbers, symbols, or empty strings. It also checks to see if Zip or City name and State is being used and gets the proper url for the forecast API call
             try:
                 if locationUrl == f"http://api.openweathermap.org/geo/1.0/direct?q={requestedLocation.replace(' ', '')},US&appid={apiKey}" and requestedLocation.replace(' ', '') != "":
                     forecastUrl = f"https://api.openweathermap.org/data/2.5/weather?lat={locationData[0]['lat']}&lon={locationData[0]['lon']}&appid={apiKey}"
@@ -48,13 +49,35 @@ def main():
 
 
 
+        if locationResponse.ok == True:
+            print(locationResponse)
+        else:
+            print("Couldnt connect to webservice")
+        print(locationData)
         print(forecastResponse)
         print(forecastData)
-
-        print(locationResponse)
-        print(locationData)
+        
+        temp = fm.getTemp(forecastData)
+        tempMax = fm.getTempMax(forecastData)
+        tempMin = fm.getTempMin(forecastData)
+        weather = fm.getWeather(forecastData)
+        
+        
+        while displayInformation == True:
+            desiredInfo = input("What information would you like to see? (Temp, Weather, Time): ").replace(' ', '')
+            
+            choiceList = desiredInfo.split(',')
+            
+            for i in choiceList:
+                if i.capitalize() == 'Temp':
+                    print(f"The current temperature is {temp} Fahrenheit\nThe High is {tempMax} and the low is {tempMin}")
+                elif i.capitalize() == 'Weather':
+                    print(f"the current weather is {weather}")
+                elif i.capitalize() == 'Time':
+                    print(fm.getTime(forecastData))
+                else:
+                    print("That information was not available")
+            
+        
+        stillUsing = False
 main()
-
-def kelvinToFahrenheit(kelvin):
-    fahrenheit = ((kelvin*9)/5) - 459.67
-    return fahrenheit
